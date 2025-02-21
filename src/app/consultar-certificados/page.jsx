237b7fae774e2c1c89baff2c5bec2a05aa6b92c6
BillAndyQ <billandyqm@gmail.com>
2025-02-21 16:38:44 -0500

    certs

---
+++ src/app/consultar-certificados/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '@/app/components/login'
+
+function Page() {
+  return (
+    <div>
+        Consulta certificados
+    </div>
+  )
+}
+
+export default Page
