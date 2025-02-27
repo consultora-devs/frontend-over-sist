01ad633359d996f376744d5e169d333dc9d40a6d
max <obregonjefferson2@gmail.com>
2025-02-27 10:06:29 -0500

    commit

--- src/app/equipos/editar/[id]/page.tsx
+++ src/app/equipos/editar/[id]/page.tsx
@@ -110,6 +110,10 @@ const EditEquipoPage: React.FC = () => {
     fetchData();
   }, [equipoId, setValue]);
 
+  if(loading){
+    <div className="spinner-border animate-spin border-t-2 border-b-2 border-white w-4 h-4 mr-2"></div>
+  }
+
   const onSubmit: SubmitHandler<FormData> = async (data) => {
     // Construimos el payload, forzando n_orden_servicio como null
     const payload = {
@@ -503,6 +507,24 @@ const EditEquipoPage: React.FC = () => {
               />
               {errors.costo_dolares && (
                 <span className="text-red-500 text-sm">{errors.costo_dolares.message}</span>
+              )}
+            </div>
+          )}
+             {isFieldVisible("comentario", tokenRole) && (
+            <div className="flex flex-col">
+              <label htmlFor="comentario" className="mb-2 text-gray-700 dark:text-gray-200">
+                comentario
+              </label>
+              <input
+                id="comentario"
+                type="text"
+                step="0.01"
+                placeholder="Ingrese comentarios"
+                {...register("comentario", { valueAsNumber: true })}
+                className="px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100"
+              />
+              {errors.comentario && (
+                <span className="text-red-500 text-sm">{errors.comentario.message}</span>
               )}
             </div>
           )}
